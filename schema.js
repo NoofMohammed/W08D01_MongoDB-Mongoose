920f658e467b989ad09cdb28f0b496baee9c7b13
Noof <noof.mohammed998@gmail.com>
2021-05-16 13:23:50 +0300

    pluse check

--- schema.js
+++ schema.js
@@ -1,1 +1,14 @@
 const mongoose = require("mongoose");
+
+
+const todoSchema = new mongoose.Schema({
+    task: { type: String,  unique: true },
+          description:{ type: String,  unique: true },
+       deadline:{ type: String,  unique: true },
+       isCompleted:{ type: String,  unique: true }, 
+        priority:{ type: String,  unique: true }
+    });
+
+    module.exports = mongoose.model("todo", todoSchema);
+
+    
